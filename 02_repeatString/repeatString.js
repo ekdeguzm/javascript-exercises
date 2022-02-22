c1e4e5ee43e3a1187a86792bb403a370f8675f1c
Kawayan <kawayan.deguzman@gmail.com>
2022-02-21 22:55:41 -0800

    Passed all tests for repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(word, number) {
+    if (number < 0) {
+        return "ERROR"
+    } else {
+    string = ""
+    
+    for (i = 0; i < number; i++) {
+        string += word
+    }
+    return string
+    }
 };
 
 // Do not edit below this line
+
 module.exports = repeatString;
