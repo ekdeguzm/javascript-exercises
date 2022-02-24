15ece4d4a03fb4e5f2c44a52c75098bd40a06e59
Kawayan <kawayan.deguzman@gmail.com>
2022-02-24 00:54:04 -0800

    Passed all of the tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,17 @@
-const ftoc = function() {
-
+const ftoc = function(far) {
+  // (f-32) * 5/9
+  far = (far-32) * 5/9
+  Math.round(far * 10) / 10
+  return Math.round(far * 10) / 10
 };
 
-const ctof = function() {
-
+const ctof = function(cel) {
+  // (c/ (9/5))+32
+  cel = (cel * (9/5)) + 32
+  Math.round(cel * 10) / 10
+  return Math.round(cel * 10) / 10
 };
+
 
 // Do not edit below this line
 module.exports = {
