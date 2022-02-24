54dcff34675ebacbaff9285c15d32c14d0764d0c
Kawayan <kawayan.deguzman@gmail.com>
2022-02-23 23:53:31 -0800

    Passed all of tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,26 @@
-const sumAll = function() {
+const sumAll = function(startNumber,finishNumber) {
+    let finalSum = 0
+    if ((startNumber < 0) || (finishNumber < 0)) {
+        return "ERROR"
+    }
+    if (typeof(startNumber) !== "number" || typeof(finishNumber) !== "number") {
+        return "ERROR"
+    }
+    if (startNumber < finishNumber) {
+        for (let i = startNumber; i <= finishNumber; i++) {
+            finalSum += i;
+        }
+    } else {
+        for (let i = startNumber; i >= finishNumber; i--) {
+            finalSum += i;
 
+        }
+    }
+    return finalSum;
+    
 };
+
+console.log(sumAll(4,1))
 
 // Do not edit below this line
 module.exports = sumAll;
