98de60eca8d63c9d2634dd35739ae526f04b188e
Kawayan <kawayan.deguzman@gmail.com>
2022-02-23 17:51:05 -0800

    passed all tests on 04_removeFromArray.js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,34 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(...args) {
+    const array = args[0]
+    const newArray = []
+    array.forEach((item) => {
+        if (!args.includes(item)) {
+            newArray.push(item);
+        }
+    })
+    return newArray;
 };
+
+
+
+
+
+([1,2,3,4,5],3,4)
+// we want to have the program search argument[0] for argument[1]
+// if the argument[1] is in the argument[0]
+
+
+
+
+
+//     for (let i = 0; i < array.length; i++) {
+//         if (array[i] === number) {
+//             array.splice(i,1)
+//         }
+//     }
+// return array
+// };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
