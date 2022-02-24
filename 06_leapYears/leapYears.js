353625454f845937cc4e50ae69d84a1b1711cb68
Kawayan <kawayan.deguzman@gmail.com>
2022-02-24 00:33:25 -0800

    Passed all tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,19 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    // leap years divisible by 4, not divisible by 100
+    if (year % 400 == 0) {
+        return true
+    }
+    if (year % 100 == 0) {
+        return false
+    }
+    if (year % 4 == 0) {
+        return true
+    } else {
+        return false
+    }
 };
+
+
 
 // Do not edit below this line
 module.exports = leapYears;
