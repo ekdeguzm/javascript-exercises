35b9c801791cdb95a4ffc4e5ac8aa9ff3f55a61c
Kawayan <kawayan.deguzman@gmail.com>
2022-03-11 10:08:16 -0800

    Pass all tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes =  str => {
+    // remove punctuation, to lower case.
+    console.log(str)
+    str = str.replace(/[!.,?:;\/() _-]/g, '').toLowerCase();
+    console.log(str)
+    // Compare the string with it's reversed version.
+    return str == str.split('').reverse().join('');
+    
 };
 
 // Do not edit below this line
