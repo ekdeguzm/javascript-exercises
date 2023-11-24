72d851c26ca08a7edcf5cfa3dd62b9019dbe7366
Kawayan <kawayan.deguzman@gmail.com>
2023-11-23 20:40:54 -0800

    add npm

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,21 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    return array.reduce((oldest, currentPerson) => {
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const currentAge = getAge(currentPerson.yearOfBirth,currentPerson.yearOfDeath);
+        console.log(currentAge)
+        return oldestAge < currentAge ? currentPerson: oldest;
+    })
 };
+
+const getAge = function(birth,death) {
+    if (!death) {
+        death = new Date().getFullYear();
+        console.log(death)
+    }
+    return death - birth;
+};
+
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
